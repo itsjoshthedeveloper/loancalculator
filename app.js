e5d0816a1605c7f0867132519985600d9275f106
Josh Chough <joshchough@gmail.com>
2020-05-16 15:34:35 -0400

    calculate results

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-alert();
+// Define UI Vars
+const form = document.querySelector('#loan-form');
+const amount = document.querySelector('#amount');
+const interest = document.querySelector('#interest');
+const years = document.querySelector('#years');
+const monthlyPayment = document.querySelector('#monthly-payment');
+const totalPayment = document.querySelector('#total-payment');
+const totalInterest = document.querySelector('#total-interest');
+const card = document.querySelector('.card');
+const heading = document.querySelector('.heading');
+
+// Load all event listeners
+loadEventListeners();
+console.log('loaded all event listeners');
+function loadEventListeners() {
+  // Add submit event
+  form.addEventListener('submit', calculateResults);
+}
+
+// Calculate results
+function calculateResults(e) {
+  console.log('Calculating...');
+
+  // Get inputs
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError('Please check your numbers');
+  }
+
+  e.preventDefault();
+}
+
+// Show error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement('div');
+  errorDiv.className = 'alert alert-danger';
+  errorDiv.textContent = error;
+
+  // Insert div above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear error
+function clearError() {
+  document.querySelector('.alert').remove();
+}
